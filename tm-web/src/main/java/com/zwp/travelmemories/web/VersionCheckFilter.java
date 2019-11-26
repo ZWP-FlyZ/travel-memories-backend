ef84c2f8e9d53ad7ddc7422c2924eb5d40c11cea
zwp <1258271806@qq.com>
2019-11-26 19:09:01 +0800

    更改通信协议版本检查由过滤器实现

---
+++ tm-web/src/main/java/com/zwp/travelmemories/web/VersionCheckFilter.java
@@ -0,0 +1,11 @@
+package com.zwp.travelmemories.web;
+
+/**
+ * @program: travelmemories
+ * @description: 通信协议检查和设置过滤器
+ * @author: zwp-flyz
+ * @create: 2019-11-26 17:55
+ * @version: v1.0
+ **/
+public class VersionCheckFilter {
+}
